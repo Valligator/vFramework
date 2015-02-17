64dfd2fe1a00930c2840135af43cf13d8410a63d
btoffel@dixonhill.com <btoffel@dixonhill.com>
2015-02-17 16:09:46 -0500

    Reverse test mod to login page

--- html/login.php
+++ html/login.php
@@ -60,7 +60,6 @@ if ((int)$thisErrCode < 0) { echo '<div style="margin-top:25px;text-align:center
   <input type="hidden" name="whereFrom" value="<?=$phpSelf;?>" />
   <input type="hidden" name="token" value="<?=$token;?>" />
  </form>
- xxx
 </div>
 <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
