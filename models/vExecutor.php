6bbe0e8c1adeb0cbcd5884018ab7aa8b4197e829
RightHandedMonkey <rooster@richcap.com>
2015-02-28 15:02:30 -0500

    Comment update

--- models/vExecutor.php
+++ models/vExecutor.php
@@ -1,7 +1,7 @@
 <?php
 require_once("vModuleInterface.php");
 
-/**
+/*
  * The executor is responsible for running the modules, and does the following operations:
  *	1. Read config file
  *  2. Read inputs for associated module execution instance
@@ -9,9 +9,13 @@ require_once("vModuleInterface.php");
  *  4. Launch module execution - using $vModule->launcher();
  *  5. Return results to the caller
  Author: sbossen btoffel
+ */
 class vExecutor {
 
+	
 	public static function executeModule($modulename, $filename, $taskId) {
+		/*
+		 * //Commented out until the other modules can be finished
 		require_once($filename);
 		$module = new $modulename;
 		vConfig::validateModuleConfig($modulename); //Throws a module configuration file error if not valid
@@ -19,7 +23,10 @@ class vExecutor {
 		$params = vParameters::getModuleParameters($taskId);
 		$config->validateModuleParameters($params); //Throws a module parameter error if a parameter is not valid
 		return $module->launcher($params);
+		 * 
+		 */
 	}
 
 }
 
+?>
