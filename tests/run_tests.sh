a2275cf664bac165f4a80b9b94339f1a790da15b
RightHandedMonkey <rooster@richcap.com>
2015-02-28 14:25:51 -0500

    allows running of all tests as a suite

---
+++ tests/run_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#performs test and displays errors if they occur
+function t {
+	echo Running: phpunit --stop-on-failure $1
+	if phpunit --stop-on-failure $1 > test_out.txt
+	then 
+		echo "passed"
+	else
+		echo "*** FAILED ***"
+		cat test_out.txt | more
+		echo Run: phpunit --stop-on-failure $1
+		exit 1
+	fi
+} 
+
+#record start
+echo Start:$(date)
+#echo $(date) > timestart.txt
+
+#tests to run
+t /usr/VALLIGATOR/vFramework/tests/vModuleDb_Test.php
+
+echo End:$(date)
+#echo $(date) > timeend.txt
