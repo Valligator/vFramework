a52dcc0405befd00f70f5e75a02e71054e131975
Bill Toffel <github@dixonhill.com>
2015-07-12 21:17:36 -0400

    Add additional helper functions

--- models/vUtils.php
+++ models/vUtils.php
@@ -14,4 +14,21 @@ class vUtils {
         list($usec, $sec) = explode(" ", microtime()); 
         return round(1000 * ((float)$usec + (float)$sec));
 	}
+
+	// base64 encode (URL safe)
+	public static function base64_url_encode($input) {
+		return strtr(base64_encode($input), '+/=', ',_-');
+	}
+
+	// base64 decode (URL safe)
+	public static function base64_url_decode($input) {
+		return base64_decode(strtr($input, ',_-', '+/='));
+	}
+
+	// generate 56-character key string (used for tokens, salts, sessions, etc.)
+	public static function genKey()
+	{
+		return base64_encode(mcrypt_create_iv(40, MCRYPT_DEV_URANDOM));
+	}
+	
 }
